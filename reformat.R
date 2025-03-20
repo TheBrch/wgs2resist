7170f9208315a828ca0f3c3cf62cccfd41d56e9f
Lukas <lukas.berzinis@proton.me>
2025-03-20 02:37:14 +0200

    Wrote an actual script that reformats input snp-core tsv file and adds labels from a separate succeptibility file

---
+++ reformat.R
@@ -0,0 +1,51 @@
+if (!require(pacman)) {
+  install.packages(
+    "pacman",
+    repos = "http://cran.us.r-project.org"
+  )
+}
+p_load(
+  readr,
+  dplyr,
+  tibble
+)
+
+file <- "../snake_out/snippy-core/PAN.tab" #args[1]
+sus <- "susceptibility.tsv" #args[2]
+
+sus_data <- read_tsv(sus, col_names = TRUE, show_col_types = FALSE) %>%
+  column_to_rownames("Antibiotic") %>%
+  select(contains("_PCD_")) %>%
+  mutate(
+    across(
+      everything(),
+      ~ ifelse(
+        grepl("^(R|I|S|R\\(Inh\\))$", .),  # Check if value contains R, I, or S
+        as.numeric(gsub('^(R|I|R\\(Inh\\))$', '0', gsub('^S$', '1', .))),
+        .
+      )
+    )
+  ) %>%
+  filter(complete.cases(.)) %>%
+  filter(!apply(., 1, function(x) all(x == x[1]))) %>%
+  t()
+
+tsv_data <- read_tsv(file, col_names = TRUE, show_col_types = FALSE) %>%
+  mutate(combined = paste(CHR, POS, sep = "-")) %>%
+  column_to_rownames("combined") %>%
+  select(-CHR, -POS, -REF) %>%
+  `colnames<-`(gsub("^PAN_", "", colnames(.))) %>%
+  t()
+
+merged_data <- merge(tsv_data, sus_data, by = "row.names") %>%
+  column_to_rownames("Row.names")
+
+write.table(
+  merged_data,
+  file = "training_matrix.tsv",
+  sep = "\t",
+  row.names = FALSE,
+  col.names = TRUE,
+  quote = FALSE
+)
+
