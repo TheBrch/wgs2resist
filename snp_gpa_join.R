3d04d1d0eec67da4a111cc0f8859af5b55787749
Lukas <lukas.berzinis@proton.me>
2025-04-17 16:32:51 +0300

    Code now joins GPA and binarized SNP data

--- snp_gpa_join.R
+++ snp_gpa_join.R
@@ -5,6 +5,7 @@ if (!require(pacman)) {
   )
 }
 p_load(
+  arrow,
   readr,
   dplyr,
   tibble,
@@ -13,3 +14,20 @@ p_load(
 
 config <- read_yaml("config.yaml")
 gpam <- config$gpam_table
+bin_data <- read_feather(args[1])
+
+gpam_data <- read_tsv(gpam, col_names = TRUE, show_col_types = FALSE) %>%
+  column_to_rownames("Gene") %>%
+  t() %>%
+  as.data.frame() %>%
+  rownames_to_column("rownames")
+
+joint <- gpam_data %>%
+  right_join(bin_data, by = "rownames") %>%
+  column_to_rownames("rownames")
+
+if (!dir.exists("./joint_data")) {
+  dir.create("./joint_data", recursive = TRUE)
+}
+
+write_feather(joint, "./joint_data/Amoxicillin_Clavulanate.feather")
