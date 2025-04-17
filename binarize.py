286e799a6090459e86ee9a675cab647013914f51
Lukas <lukas.berzinis@proton.me>
2025-04-17 16:31:43 +0300

    Preserve rownames

--- binarize.py
+++ binarize.py
@@ -11,8 +11,9 @@ data_file = sys.argv[1]
 antibiotic_name = data_file.split("/")[-1].split(".")[0]
 data = pd.read_csv(data_file, sep="\t")
 
+rownames = data[["rownames"]]
 y = data[["Susceptible"]]
-X = data.drop(columns=["Susceptible"])
+X = data.drop(columns=["Susceptible", "rownames"])
 
 nuc_encoding = {
     'A': [0, 0],
@@ -24,7 +25,7 @@ nuc_encoding = {
 def encode_nuc(s):
     return nuc_encoding.get(s, [None, None])
 
-new_cols = []
+new_cols = [rownames]
 for col in X.columns:
     encoded = X[col].apply(encode_nuc)
     new_cols.append(X[col].apply(lambda x: encode_nuc(x)[0]).rename(f'{col}_bit1'))
@@ -34,5 +35,5 @@ X_bin = pd.concat(new_cols, axis=1)
 
 os.makedirs("binarized_data", exist_ok=True)
 
-X_bin.to_pickle(f"binarized_data/{antibiotic_name}.pkl")
+X_bin.to_feather(f"binarized_data/{antibiotic_name}.feather")
 y.to_pickle(f"binarized_data/{antibiotic_name}_lab.pkl")
