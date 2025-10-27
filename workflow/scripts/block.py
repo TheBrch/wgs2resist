34c2c48340ebae2f46b5a12a9a3dd523270cacc8
Lukas Beržinis <lukas.berzinis@gmc.stud.vu.lt>
2025-10-27 21:28:03 +0200

    Removed debug prints, wrapped blocking into a function that returns a set of bins with patient id's, and a dictionary with statistic values of binning results. Bin count is set to count of positive patients.

--- workflow/scripts/block.py
+++ workflow/scripts/block.py
@@ -1,4 +1,4 @@
-bincount = 5
+import statistics as st
 
 meta = {
     "s00": {"patient": "P001", "susceptible": 1},
@@ -28,32 +28,48 @@ meta = {
 # 0	2	2	6	1	1	1
 # 						20
 
-patients = {}
-for sample_id, info in meta.items():
-    pid = info["patient"]
-    if pid not in patients:
-        patients[pid] = {"samples": [], "pos": 0, "neg": 0}
 
-    patients[pid]["samples"].append(sample_id)
-    if info["susceptible"] == 1:
-        patients[pid]["pos"] += 1
-    else:
-        patients[pid]["neg"] += 1
+def blocking(meta):
+    patients = {}
+    for sample_id, info in meta.items():
+        pid = info["patient"]
+        if pid not in patients:
+            patients[pid] = {"samples": [], "pos": 0, "neg": 0}
 
-patient_list = [{**info, "pid": pid} for pid, info in patients.items()]
+        patients[pid]["samples"].append(sample_id)
+        if info["susceptible"] == 1:
+            patients[pid]["pos"] += 1
+        else:
+            patients[pid]["neg"] += 1
 
-print(patient_list, "\n")
+    patient_list = [{**info, "pid": pid} for pid, info in patients.items()]
 
-patient_list.sort(key=lambda x: (-len(x["samples"]), -x["pos"]))
+    patient_list.sort(key=lambda x: (-len(x["samples"]), -x["pos"]))
 
-print(patient_list, "\n")
+    positive_patients = sum(x["pos"] > 0 for x in patient_list)
+    print(f"Bin count: {positive_patients}")
 
-bins = [{"no": i, "size": 0, "pos": 0, "patients": []} for i in range(bincount)]
+    bins = [{"size": 0, "pos": 0, "patients": []} for _ in range(positive_patients)]
 
-for patient in patient_list:
-    bins.sort(key=lambda x: (x["pos"], x["size"]))
-    bins[0]["size"] += len(patient["samples"])
-    bins[0]["pos"] += patient["pos"]
-    bins[0]["patients"].append(patient["pid"])
+    for patient in patient_list:
+        bins.sort(key=lambda x: (x["pos"], x["size"]))
+        bins[0]["size"] += len(patient["samples"])
+        bins[0]["pos"] += patient["pos"]
+        bins[0]["patients"].append(patient["pid"])
 
-print(*bins, sep="\n")
+    bin_sizes = [x["size"] for x in bins]
+    bin_ratios = [x["pos"] / x["size"] for x in bins]
+    bin_stats = {
+        "r_mean": st.mean(bin_ratios),
+        "r_stdev": st.stdev(bin_ratios),
+        "s_mean": st.mean(bin_sizes),
+        "s_stdev": st.stdev(bin_sizes),
+    }
+    bin_stats["s_percent"] = 100 * bin_stats["s_stdev"] / bin_stats["s_mean"]
+
+    return bins, bin_stats
+
+
+b, b_s = blocking(meta)
+print(*b, sep="\n")
+print(b_s)
