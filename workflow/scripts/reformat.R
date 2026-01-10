a181f2829526b3d38862343b069e085a1f384f63
Lukas Beržinis <lukas.berzinis@gmc.stud.vu.lt>
2026-01-10 18:35:16 +0200

    Renamed some variables to make code more readable. Added code that processes unitig tsv files.

--- workflow/scripts/reformat.R
+++ workflow/scripts/reformat.R
@@ -8,13 +8,15 @@ p_load(
   readr,
   dplyr,
   tibble,
-  yaml
+  yaml,
+  arrow
 )
 
 config <- read_yaml(file.path("config", "config.yaml"))
 
 args <- commandArgs(trailingOnly = TRUE)
-file <- args[1]
+snvs <- args[1]
+unitigs <- args[2]
 sus <- config$suscept_table
 
 sus_data <- read_tsv(sus, col_names = TRUE, show_col_types = FALSE) %>%
@@ -43,11 +45,16 @@ sus_data <- sus_data[, valid]
 
 print(colSums(!is.na(sus_data)))
 
-tsv_data <- read_tsv(file, col_names = TRUE, show_col_types = FALSE) %>%
+snv_data <- read_tsv(snvs, col_names = TRUE, show_col_types = FALSE) %>%
   mutate(combined = paste(CHR, POS, sep = "-")) %>%
   column_to_rownames("combined") %>%
   select(-CHR, -POS, -REF) %>%
-  `colnames<-`(gsub("^PAN_", "", colnames(.))) %>%
+  t() %>%
+  as.data.frame() %>%
+  rownames_to_column("rownames")
+
+unitig_data <- read_tsv(unitigs, col_names = TRUE, show_col_types = FALSE) %>%
+  column_to_rownames("Unitig_sequence") %>%
   t() %>%
   as.data.frame() %>%
   rownames_to_column("rownames")
@@ -56,23 +63,31 @@ if (!dir.exists(file.path("results", "training_data"))) {
   dir.create(file.path("results", "training_data"), recursive = TRUE)
 }
 
+if (!dir.exists(file.path("results", "joint_data"))) {
+  dir.create(file.path("results", "joint_data"), recursive = TRUE)
+}
+
 for (i in colnames(sus_data)) {
   n <- gsub("\\/", "_", i)
   fn <- file.path("results", "training_data", paste0(n, ".tsv"))
-  data <- sus_data %>%
+  fn_uni <- file.path("results", "joint_data", paste0(n, "_unitig.feather"))
+  is_susceptible <- sus_data %>%
     select(all_of(i)) %>%
     na.omit() %>%
     rename("Susceptible" = i) %>%
     rownames_to_column("rownames")
-  merged_data <- data %>%
-    left_join(tsv_data, by = "rownames")
-  assign(n, merged_data)
+  merged_snv_data <- is_susceptible %>%
+    left_join(snv_data, by = "rownames")
   write.table(
-    merged_data,
+    merged_snv_data,
     file = fn,
     sep = "\t",
     row.names = FALSE,
     col.names = TRUE,
     quote = FALSE
   )
+  merged_unitig_data <- is_susceptible %>%
+    left_join(unitig_data, by = "rownames") %>%
+    select(-Susceptible)
+  write_feather(merged_unitig_data, fn_uni)
 }
